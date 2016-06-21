3dd797b4002a1fee5b248479d4c15c0fd20f641d
Pascal van Kooten <kootenpv@gmail.com>
2016-06-22 00:24:26 +0200

    applied pep8 (2)

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
-import uci_loader, numpy as np
+import uci_loader
+import numpy as np
 from decisionboundaryplot import DBPlot
 from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.linear_model.logistic import LogisticRegression
@@ -14,30 +15,33 @@ if __name__ == "__main__":
     X, y = uci_loader.getdataset('heart')
     #data = load_digits(n_class = 2)
     #X,y = data.data, data.target
-    y[y!=0] = 1
-    
+    y[y != 0] = 1
+
     # shuffle data
     random_idx = permutation(np.arange(len(y)))
     X = X[random_idx]
     y = y[random_idx]
-    
+
     # create model
     model = LogisticRegression(C=1)
     #model = RandomForestClassifier(n_estimators=10)
-    
+
     # plot high-dimensional decision boundary
     db = DBPlot(model)
     db.fit(X, y, training_indices=0.5)
-    db.plot(plt, generate_testpoints=True) # set generate_testpoints=False to speed up plotting
+    db.plot(plt, generate_testpoints=True)  # set generate_testpoints=False to speed up plotting
     plt.show()
-    
-    #plot learning curves for comparison
+
+    # plot learning curves for comparison
     N = 10
-    train_sizes, train_scores, test_scores = learning_curve(model, X, y, cv=5, train_sizes=np.linspace(.2, 1.0, N))
-    
-    plt.errorbar(train_sizes, np.mean(train_scores, axis=1), np.std(train_scores, axis=1)/np.sqrt(N))
-    plt.errorbar(train_sizes, np.mean(test_scores, axis=1), np.std(test_scores, axis=1)/np.sqrt(N), c='r')
-    
+    train_sizes, train_scores, test_scores = learning_curve(
+        model, X, y, cv=5, train_sizes=np.linspace(.2, 1.0, N))
+
+    plt.errorbar(train_sizes, np.mean(train_scores, axis=1),
+                 np.std(train_scores, axis=1) / np.sqrt(N))
+    plt.errorbar(train_sizes, np.mean(test_scores, axis=1),
+                 np.std(test_scores, axis=1) / np.sqrt(N), c='r')
+
     plt.legend(["Accuracies on training set", "Accuracies on test set"])
     plt.xlabel("Number of data points")
     plt.title(str(model))
