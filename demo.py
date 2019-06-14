f0a9b2963080fb4dd5534b3cb4dc730bea6d0e59
rednafi <redowan.nafi@gmail.com>
2019-06-15 02:35:26 +0600

    code style: black

--- demo.py
+++ demo.py
@@ -12,9 +12,9 @@ from sklearn.neighbors import KNeighborsClassifier
 
 if __name__ == "__main__":
     # load data
-    X, y = uci_loader.getdataset('iris')
-    #data = load_digits(n_class = 2)
-    #X,y = data.data, data.target
+    X, y = uci_loader.getdataset("iris")
+    # data = load_digits(n_class = 2)
+    # X,y = data.data, data.target
     y[y != 0] = 1
 
     # shuffle data
@@ -23,23 +23,31 @@ if __name__ == "__main__":
     y = y[random_idx]
 
     # create model
-    model = LogisticRegression(C = 1)
-    #model = RandomForestClassifier(n_estimators=10)
+    model = LogisticRegression(C=1)
+    # model = RandomForestClassifier(n_estimators=10)
 
     # plot high-dimensional decision boundary
     db = DBPlot(model)
-    db.fit(X, y, training_indices = 0.5)
-    db.plot(plt, generate_testpoints = True )  # set generate_testpoints=False to speed up plotting
+    db.fit(X, y, training_indices=0.5)
+    db.plot(
+        plt, generate_testpoints=True
+    )  # set generate_testpoints=False to speed up plotting
     plt.show()
 
     # plot learning curves for comparison
     N = 10
-    train_sizes, train_scores, test_scores = learning_curve(
-        model, X, y, cv = 5)
-    plt.errorbar(train_sizes, np.mean(train_scores, axis = 1),
-                 np.std(train_scores, axis = 1) / np.sqrt(N))
-    plt.errorbar(train_sizes, np.mean(test_scores, axis = 1),
-                 np.std(test_scores, axis = 1) / np.sqrt(N), c = 'r')
+    train_sizes, train_scores, test_scores = learning_curve(model, X, y, cv=5)
+    plt.errorbar(
+        train_sizes,
+        np.mean(train_scores, axis=1),
+        np.std(train_scores, axis=1) / np.sqrt(N),
+    )
+    plt.errorbar(
+        train_sizes,
+        np.mean(test_scores, axis=1),
+        np.std(test_scores, axis=1) / np.sqrt(N),
+        c="r",
+    )
 
     plt.legend(["Accuracies on training set", "Accuracies on test set"])
     plt.xlabel("Number of data points")
--- uci_loader.py
+++ uci_loader.py
@@ -24,7 +24,7 @@ def tonumeric(lst):
     return np.array([lbls[unpack(t)] for t in lst.flatten()])
 
 
-def getdataset(datasetname, onehot_encode_strings = True):
+def getdataset(datasetname, onehot_encode_strings=True):
     # load
     dataset = fetch_mldata(datasetname)
     # get X and y
@@ -35,21 +35,27 @@ def getdataset(datasetname, onehot_encode_strings = True):
         print("WARNING: No target found. Taking last column of data matrix as target")
         target = X[:, -1]
         X = X[:, :-1]
-    if len(target.shape) > 1 and target.shape[1] > X.shape[1]:  # some mldata sets are mixed up...
+    if (
+        len(target.shape) > 1 and target.shape[1] > X.shape[1]
+    ):  # some mldata sets are mixed up...
         X = target
         target = dshape(dataset.data)
     if len(X.shape) == 1 or X.shape[1] <= 1:
         for k in dataset.keys():
-            if k != 'data' and k != 'target' and len(dataset[k]) == X.shape[1]:
+            if k != "data" and k != "target" and len(dataset[k]) == X.shape[1]:
                 X = np.hstack((X, dshape(dataset[k])))
     # one-hot for categorical values
     if onehot_encode_strings:
-        cat_ft = [i for i in range(X.shape[1]) if 'str' in str(
-            type(unpack(X[0, i]))) or 'unicode' in str(type(unpack(X[0, i])))]
+        cat_ft = [
+            i
+            for i in range(X.shape[1])
+            if "str" in str(type(unpack(X[0, i])))
+            or "unicode" in str(type(unpack(X[0, i])))
+        ]
         if len(cat_ft):
             for i in cat_ft:
                 X[:, i] = tonumeric(X[:, i])
-            X = OneHotEncoder(categorical_features = cat_ft).fit_transform(X)
+            X = OneHotEncoder(categorical_features=cat_ft).fit_transform(X)
     # if sparse, make dense
     try:
         X = X.toarray()
